ef56c1d05535f2a28ad7b78ba0df5974f8d3e5fb
Nguyen Duc Manh <izayoizero01@gmail.com>
2026-09-14 23:44:57 +0700

    Gameplay

--- main.gd
+++ main.gd
@@ -3,23 +3,26 @@ extends Node
 @export var mob_scene: PackedScene
 var score
 func _ready() -> void:
-	new_game()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(	delta: float) -> void:
+func _process(	_delta: float) -> void:
 	pass
 
 
 func game_over() -> void:
+	$Player.hide()
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	$GameOverTimer.start()
 	
 func new_game():
 	score = 0
-	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 
 func _on_mob_timer_timeout() -> void:
@@ -29,8 +32,7 @@ func _on_mob_timer_timeout() -> void:
 	# Spawn position
 	mob.position = mob_spawn_location.position
 	# Direction
-	#var direction = mob_spawn_location.rotation + PI / 2
-	var direction = mob_spawn_location.rotation
+	var direction = mob_spawn_location.rotation + PI / 2
 	direction += randf_range(-PI / 4, PI / 4)
 	mob.rotation = direction
 	# Velocity
@@ -40,8 +42,17 @@ func _on_mob_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score += 1
-
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout() -> void:
 	$MobTimer.start()
 	$ScoreTimer.start()
+	$Player.start($StartPosition.position)
+
+
+func _on_hud_start_game() -> void:
+	new_game()
+
+
+func _on_game_over_timer_timeout() -> void:
+	get_tree().call_group("mobs", "queue_free")	
--- player.gd
+++ player.gd
@@ -37,8 +37,8 @@ func _process(delta: float) -> void:
 func start(pos):
 	position = pos
 	show()
-	$CollisionShape2D.disabled = false
+	$CollisionShape2D.set_deferred("disabled", false)
 	
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
 	hit.emit()
-	$CollisionShape2D.set_defered("disabled", true)
+	$CollisionShape2D.set_deferred("disabled", true)
