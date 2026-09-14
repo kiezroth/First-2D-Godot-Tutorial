d661e9a6c467539f673f863bfdd1cc8297db24fb
Nguyen Duc Manh <izayoizero01@gmail.com>
2026-09-14 22:41:21 +0700

    Initial commit

---
+++ player.gd
@@ -0,0 +1,44 @@
+extends Area2D
+
+@export var speed = 400
+signal hit()
+var screen_size
+func _ready() -> void:
+	hide()
+	screen_size = get_viewport_rect().size
+
+
+func _process(delta: float) -> void:
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity != Vector2.ZERO:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+func respawn(pos):
+	position = pos
+	show()
+	$CollisionShape2D.set_defered("disabled", false)
+	
+func _on_body_entered(body: Node2D) -> void:
+	hit.emit()
+	$CollisionShape2D.set_defered("disabled", true)
